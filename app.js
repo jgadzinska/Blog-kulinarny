181e036ae858d8eec52d212fde77f7bad0c50c52
jgadzinska <joanna.gadzinska88@wp.pl>
2024-01-25 09:09:45 +0100

    New app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const searchOn = document.querySelector('#searchOpen');
+const searchOff = document.querySelector('#removeSearch');
+const searchInput = document.querySelector('.searchInput');
+
+searchInput.style.display = "none";
+
+searchOn.addEventListener('click', () => {
+    if(searchInput.style.display === 'none') {
+        searchInput.style.display = 'flex';
+    } else {
+        searchInput.style.display = 'none';
+    }
+});
+
+searchOff.addEventListener('click', () => {
+    if(searchInput.style.display === 'flex') {
+        searchInput.style.display = 'none';
+    } else {
+        searchInput.style.display = 'flex';
+    }
+});
