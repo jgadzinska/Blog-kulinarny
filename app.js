dbdb7b8ef9ac0714c38010b59b35247b5f01e2f3
jgadzinska <joanna.gadzinska88@wp.pl>
2024-01-31 10:27:25 +0100

    New app.js

--- app.js
+++ app.js
@@ -20,29 +20,3 @@ searchOff.addEventListener('click', () => {
     }
 });
 
-
-const posts = [
-    {
-        title: "Browni",
-        desc: "Zobacz jak upiec wilgotne i mocno czekoladowe brownie. A do tego sposób przygotowania jest bardzo proste!",
-        link: "https://google.com/",
-        bgImg: "brownie.jpeg",
-        label: "Ciasta"
-    },
-    {
-        title: "Sałatka grecka",
-        desc: "Sałatka grecka ze świeżych warzyw, wyrazistych w smaku czarnych oliwek i sera fety, który jest uważany za najlepszy kulinarny wynalazek greckiej kuchni!",
-        link: "https://google.com/",
-        bgImg: "salatkaGrecka.jpeg",
-        label: "Sałatki"
-    },
-    {
-        title: "Paella",
-        desc: "Zanurz się w świecie hiszpańskich smaków z naszym przepisem na tradycyjną paellę. Z soczystym kurczakiem oraz aromatycznymi warzywami!",
-        link: "https://google.com/",
-        bgImg: "paella.png",
-        label: "Dania główne"
-    }
-];
-
-let currentSlider = 0;
